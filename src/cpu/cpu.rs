f04590b5bc2a12b1024a232374f7b5fd7a4f31b3
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2026-01-15 21:14:12 -0300

    (feat) new opcode inc_a

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -20,7 +20,6 @@ bitflags! {
 pub struct Cpu {
     pub register_a: u8,
     pub register_f: Flags,
-
     pub register_b: u8,
     pub register_c: u8,
     pub register_d: u8,
@@ -40,29 +39,7 @@ pub struct Cpu {
     pub memory_bus: MemoryBus
 }
 
-// impl Default for Cpu {
-//     fn default() -> Self {
-//         Self {
-//             register_a: 0,
-//             register_f: Flags::empty(),
-//             register_b: 0,
-//             register_c: 0,
-//             register_d: 0,
-//             register_e: 0,
-//             register_h: 0,
-//             register_l: 0,
-//
-//             stack_pointer: 0,
-//             program_counter: 0,
-//
-//             halt: false,
-//             interruption: false,
-//
-//             opcode: 0,
-//             cycles: 0
-//         }
-//     }
-// }
+
 
 impl Cpu {
     pub fn new(bus: MemoryBus) -> Self {
@@ -87,8 +64,30 @@ impl Cpu {
             memory_bus: bus
         }
     }
+ 
+    // ADD magic values to ignore the gameboy bootrom
+    pub fn init_registers(&mut self){
+
+        self.register_a = 0x01;
+        self.register_b = 0x00;
+        self.register_c = 0x13;
+        self.register_d = 0x00;
+        self.register_e = 0xD8;
+        self.register_h = 0x01;
+        self.register_l = 0x4D;
+
+        self.program_counter = 0x0100;
+        self.stack_pointer = 0xFFFE;
+
+        if(self.memory_bus.cartridge.header_checksum == 0x00){
+            self.register_f = Flags::Z;
+        } else {
+            self.register_f = Flags::Z | Flags::H | Flags::C;
+        }
+    }
 
     pub fn start(&mut self) {
+        self.init_registers();
         self.clock();
     }
 
@@ -132,6 +131,7 @@ impl Cpu {
         match inst {
             0x00 => self.NOP(),
             0x01 => self.LD_BC_u16(),
+            0xC3 => self.INC_A(),
             0x10 => self.STOP(),
             0xFF => self.RST(),
             _ => todo!("Instrução ainda não implementada: 0x{:02X}", inst),
@@ -161,6 +161,31 @@ impl Cpu {
         self.update_cycles(4);
     }
 
+    fn INC_A(&mut self) {
+        self.increment_program_counter();
+        
+        self.register_f.remove(Flags::H);
+
+        let a: u8 = self.register_a;
+        let result = a.wrapping_add(1);
+
+        if(result == 0x00) {
+            self.register_f.insert(Flags::Z);
+        } else {
+            self.register_f.remove(Flags::Z);
+        }
+
+        if((result & 0x0F) == 0x00){
+            self.register_f.insert(Flags::H);
+        } else {
+            self.register_f.remove(Flags::H);
+        }
+
+        self.update_cycles(4);
+
+        self.register_a = a;
+    }
+
     fn RST(&mut self) {
        self.increment_program_counter(); 
     }
@@ -169,8 +194,17 @@ impl Cpu {
         return self.memory_bus.read(addr);
     }
 
-    fn push(&mut self, &mut pc: u16) {
 
+    fn push(&mut self, pc: u16) {
+        let upper: u8 = (pc >> 8 & 0xffff) as u8;
+        let lower: u8 = (pc & 0xffff) as u8;
+
+        self.stack_pointer -= 1;
+        self.memory_bus.write(self.stack_pointer, upper);
+        
+
+        self.stack_pointer -= 1;
+        self.memory_bus.write(self.stack_pointer, lower);
     }
 
 }
